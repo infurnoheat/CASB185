bfddf765f05c08e4c18ce2ea4b45337c3cefac13
infurnoheat <stevenleeyu@gmail.com>
2023-03-04 00:53:49 -0800

    New Data

--- analyze.R
+++ analyze.R
@@ -16,18 +16,24 @@ library(ggplot2)
 library(sleuth)
 library(biomaRt)
 library(rhdf5)
+library(Rhdf5lib)
 
 #Set WD
 setwd("C:/Users/steve/CodeSpace/CASB185")
 
+#Ignoring problematic H5 Files
+
+
 #Setting directory to Kallisto Output Folder
-base_dir <- "C:/Users/steve/CodeSpace/CASB185"
+base_dir <- "."
 sample_id <- dir(file.path(base_dir, "kallisto_output"))
 sample_id
 
 #Setting directory to COVID data
 kal_dirs <- sapply(sample_id, function(id) file.path(base_dir, "kallisto_output", id))
-kal_dirs
+#kal_dirs_tsv <- paste0(kal_dirs, "/abundance.tsv")
+
+list.files(kal_dirs)
 
 #Loading Design Matrix
 s2c <- read.table("C:/Users/steve/CodeSpace/CASB185/design.txt", header = TRUE, stringsAsFactors = FALSE)
@@ -35,16 +41,15 @@ s2c <- dplyr::mutate(s2c, path = kal_dirs)
 s2c
 
 #Loading Ensembl
-mart <- biomaRt::useMart(biomart = "ENSEMBL_MART_ENSEMBL",
-                         dataset = "hsapiens_gene_ensembl",
-                         host = 'ensembl.org')
+mart <- biomaRt::useMart(biomart = "ENSEMBL_MART_ENSEMBL", dataset = "hsapiens_gene_ensembl", host = 'ensembl.org')
 
 t2g <- biomaRt::getBM(attributes = c("ensembl_transcript_id", "ensembl_gene_id", "external_gene_name"), mart = mart)
-t2g <- dplyr::rename(t2g, target_id = ensembl_transcript_id,
-                     ens_gene = ensembl_gene_id, ext_gene = external_gene_name)
+t2g <- dplyr::rename(t2g, target_id = ensembl_transcript_id,ens_gene = ensembl_gene_id, ext_gene = external_gene_name)
 
 #Running Sleuth
-H5Fopen("C:/Users/steve/CodeSpace/CASB185/kallisto_output/GSM4432378/abundance.h5")
+test1 <- H5Fopen("./kallisto_outputOLD/GSM4432378/abundance.h5")
+test2 <- H5Fopen("C:/Users/steve/Downloads/abundance.h5")
+test3 <- H5Fopen("C:/Users/steve/Downloads/moc_1_1_output/abundance.h5")
 so <- sleuth_prep(s2c, ~ condition, target_mapping = t2g, num_cores = 1)
 so <- sleuth_fit(so)
 so <- sleuth_fit(so, ~1, 'reduced')
